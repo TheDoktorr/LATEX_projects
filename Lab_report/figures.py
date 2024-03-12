ca0d03f92c91978572752d900a7c7ca2eb27e82e
hamilla1 <andrewhamill8@gmail.com>
2024-03-12 15:33:16 +0000

    graphing changes

--- Lab_report/figures.py
+++ Lab_report/figures.py
@@ -5,7 +5,11 @@ import scipy.optimize as opt
 #   array_binned = np.array([[0,1,2,3,4,5,6,7,8],[1,14,25,18,20,11,6,4,1]])
 #   np.savetxt("binned_data.csv", array_binned, delimiter=",")
 
-list_new = np.loadtxt("bg_raw.csv")
-print(list_new)
+array_new = np.loadtxt('bg_raw_3.prn')
+print(array_new)
 
-
+plt.hist(array_new)
+plt.xlabel("Count")
+plt.ylabel("frequency")
+plt.title("Counts in a 3s interval")
+plt.show()
