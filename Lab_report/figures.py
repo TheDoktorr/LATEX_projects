8cfa6384297ee416b75c563cd0b8764e1ba34b2a
hamilla1 <andrewhamill8@gmail.com>
2024-03-19 15:23:28 +0000

    updated graphs, added second scale

--- Lab_report/figures.py
+++ Lab_report/figures.py
@@ -13,33 +13,48 @@ print(array_new)
 def fit_func(k, lamb):
     return poisson.pmf(k, lamb)
 
-bins = np.arange(10) - 0.5
-print(bins)
+fig, ax1 = plt.subplots()
 
-plt.figure(figsize=(10,10))
-entries, bin_edges, patches = plt.hist(array_new, range=(0,8), bins=bins,density=True, label="Counts")
-bin_centers = 0.5 * (bin_edges[1:] + bin_edges[:-1])
-
-
-plt.xlabel("Count")
-plt.ylabel("frequency")
-plt.title("Counts in a 3s interval")
-
-mean_raw = np.mean(array_new)
-print(mean_raw)
-
-parameters, cov_matrix = curve_fit(fit_func, bin_centers, entries)
 
 x = np.arange(0, 10)
 plt.xticks(np.arange(min(x), max(x)+1, 1.0))
-plt.plot(x,
-         fit_func(x, *parameters),
-         marker='o',
-         label='fit',
-)
 
-plt.legend()
-plt.tight_layout()
+bins = np.arange(10) - 0.5
+
+color = 'tab:blue'
+ax1.set_xlabel('Count')
+ax1.set_ylabel('frequency', color=color)
+entries, bin_edges, patches =ax1.hist(array_new, range=(0,8), bins=bins, density=True, color=color, label="Counts")
+bin_centers = 0.5 * (bin_edges[1:] + bin_edges[:-1])
+ax1.tick_params(axis='y', labelcolor = color)
+
+# print(bins)
+parameters, cov_matrix = curve_fit(fit_func, bin_centers, entries)
+
+ax2 = ax1.twinx()
+
+color ='tab:red'
+ax2.set_ylabel('probability', color=color)
+ax2.plot(x, fit_func(x, *parameters), marker='o', label='fit', color = color)
+ax2.tick_params(axis='y', labelcolor = color)
+
+# mean_raw = np.mean(array_new)
+# print(mean_raw)
+
+# plt.xlabel("Count")
+# plt.ylabel("frequency")
+# plt.title("Counts in a 3s interval")
+
+# plt.plot(x,
+#        fit_func(x, *parameters),
+#       marker='o',
+#         label='fit',
+#)
+
+
+fig.legend()
+fig.tight_layout()
+fig.set_size_inches(10,10)
 plt.savefig('histogram_new.svg')
 plt.show()
 
